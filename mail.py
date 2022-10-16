1543328e05502a4b0c6b6607543e1017c9a5a598
eas27 <80135807+eas27@users.noreply.github.com>
2022-10-16 01:30:02 -0400

    improving mail function

--- mail.py
+++ mail.py
@@ -1,20 +1,20 @@
 import smtplib
 from email.message import EmailMessage
-def send_mail(to_email, subject, message, server='smtp.example.cn',
-              from_email='xx@example.com'):
-    # import smtplib
+def send_mail(to_email, subject, message, server='mail.signelix.com',
+              from_email='<>'):
+
     msg = EmailMessage()
     msg['Subject'] = subject
     msg['From'] = from_email
     msg['To'] = ', '.join(to_email)
     msg.set_content(message)
-    print(msg)
-    server = smtplib.SMTP(server)
+    server = smtplib.SMTP(server, 26)
     server.set_debuglevel(1)
-    server.login(from_email, 'password')  # user & password
+    pswd = input ('password')
+    server.login('<>', pswd)  # user & password
     server.send_message(msg)
     server.quit()
     print('successfully sent the mail.')
 
-send_mail(to_email=['eric@citadeldiscovery.io'],
+send_mail(to_email=['<>'],
           subject='hello', message='test e-mail!')
