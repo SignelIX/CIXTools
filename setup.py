bf88514e0d35718f84b0aad1e58653867c5f991c
eas27 <80135807+eas27@users.noreply.github.com>
2023-01-24 00:47:42 -0500

    PyPi install changes

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
-
+requirements = []
+with open("requirements.txt", "r") as fh2:
+    for line in fh2:
+        requirements.append(line.strip())
 setuptools.setup(
     name="CIxTools",
     version="1.0.0",
@@ -13,9 +16,10 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/<>/<>",
     packages=setuptools.find_packages(),
-    classifiers=(
+    install_requires = requirements,
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ),
+    ]
 )
